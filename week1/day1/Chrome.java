4c411a6a6f1e747f7431569ad32894c322cfc740
suji2801 <suja2mail@gmail.com>
2025-03-02 12:50:17 +0530

    Add files via upload
    Week1 Day1 Assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Chrome {
+	
+	public static void main(String[] args) {
+		
+		String developer="Google";
+		
+		float Version=91.0f;
+		
+		boolean isBeta=false;
+		
+		int releaseYear=2008;
+		
+		char shortcutKey='C';
+		
+		System.out.println("Developer of Chrome:"+developer);
+		
+		System.out.println("Version:"+Version);
+		
+		System.out.println("Is chrome is Beta version:"+isBeta);
+		
+		System.out.println("Release year:"+releaseYear);
+		
+		System.out.println("Shortcut Key:"+shortcutKey);
+	}
+	
+	
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		String browserName="Firefox";
+		
+		float browserVersion=100.2f;
+		
+		int releaseYear=1998;
+		
+		char browserLogo='f';
+		
+		boolean isVisible=true;
+		
+		System.out.println("Name of the Browser:"+browserName);
+		System.out.println("Version of the Browser:"+browserVersion);
+		System.out.println("Release Year:"+releaseYear);	
+		System.out.println("Logo of the browser:"+browserLogo);
+		System.out.println("Is browser visible:"+isVisible);
+	}
+
+}
+ 
